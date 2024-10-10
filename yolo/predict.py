b00b6ad0c86838ff05caac455814170d68cfedbf
Trần Bá Hiến <69721832+tranhien1612@users.noreply.github.com>
2024-10-10 15:02:01 +0700

    Update predict.py

--- yolo/predict.py
+++ yolo/predict.py
@@ -1,10 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO("best.pt")
+model = YOLO("/home/maj/yolo/item/runs/my_run/weights/best.pt")
 
-model.predict(source = "1.jpg", 
+model.predict(source = "/home/maj/yolo/item/valid/images/bottles-of-global-soft-drink-brands-including-products-of-coca-cola-company-and-pepsico-WX0H4K_jpg.rf.4d0122f77473252d0b89cfa2611deca5.jpg", 
+    project ="/home/maj/yolo/item/runs",
+    name = "predict",
     show=True, save=True, 
     conf=0.6, 
-    line_thickness = 1, 
+    line_thickness = 2, 
     save_crop=True
     )# , hide_labels=True, hide_conf=True)
