3441c2a9e28ab2e5180c61031d775c9394b786ce
Trần Bá Hiến <69721832+tranhien1612@users.noreply.github.com>
2024-12-05 09:12:44 +0700

    Add files via upload

---
+++ tflite/tf.py
@@ -0,0 +1,91 @@
+# Import packages 
+'''
+pip3 install tensorflow
+wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
+unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip -d TFLite_model
+'''
+import os
+import argparse
+import cv2
+import numpy as np
+import sys
+import importlib.util
+from tensorflow.lite.python.interpreter import Interpreter
+from tensorflow.lite.python.interpreter import load_delegate
+
+model_path = "TFLite_model/detect.tflite"
+lable_path = "TFLite_model/labelmap.txt"
+
+with open(lable_path, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+if labels[0] == '???':
+    del(labels[0])
+
+interpreter = Interpreter(model_path=model_path)
+interpreter.allocate_tensors()
+
+# Get model details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+floating_model = (input_details[0]['dtype'] == np.float32)
+input_mean = 127.5
+input_std = 127.5
+
+outname = output_details[0]['name'] #TF2 or TF1
+if ('StatefulPartitionedCall' in outname):
+    boxes_idx, classes_idx, scores_idx = 1, 3, 0
+else:
+    boxes_idx, classes_idx, scores_idx = 0, 1, 2
+
+# Open video file
+def main(filename):
+    video = cv2.VideoCapture(filename)
+    imW = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+    imH = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    while(video.isOpened()):
+        ret, frame = video.read()
+        if ret:
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_resized = cv2.resize(frame_rgb, (width, height))
+            input_data = np.expand_dims(frame_resized, axis=0)
+
+            if floating_model:
+                input_data = (np.float32(input_data) - input_mean) / input_std
+
+            interpreter.set_tensor(input_details[0]['index'],input_data)
+            interpreter.invoke()
+
+            boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]
+            classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]
+            scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]
+
+            for i in range(len(scores)):
+                if ((scores[i] > 0.6) and (scores[i] <= 1.0)):
+                    ymin = int(max(1,(boxes[i][0] * imH)))
+                    xmin = int(max(1,(boxes[i][1] * imW)))
+                    ymax = int(min(imH,(boxes[i][2] * imH)))
+                    xmax = int(min(imW,(boxes[i][3] * imW)))
+                    
+                    cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 4)
+
+                    # Draw label
+                    object_name = labels[int(classes[i])]
+                    label = '%s: %d%%' % (object_name, int(scores[i]*100))
+                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+                    label_ymin = max(ymin, labelSize[1] + 10)
+                    cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                    cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) 
+
+
+            cv2.imshow('Object detector', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main("test.mp4")
